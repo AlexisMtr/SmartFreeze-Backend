e1311fee94a212921a5368f6bbcb6dbbac561a8e
AlexisMtr <a.martinier@gmail.com>
2018-02-10 23:13:38 +0100

    Add ping route

---
+++ SmartFreeze/Controllers/InfosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SmartFreeze.Controllers
+{
+    [Route("api")]
+    public class InfosController : Controller
+    {
+        [HttpGet("ping")]
+        public async Task<IActionResult> Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
