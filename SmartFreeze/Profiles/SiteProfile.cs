290f9593c2bad904cb42fd817f4839364b55a094
AlexisMtr <a.martinier@gmail.com>
2018-02-14 14:47:06 +0100

    Fix activeAlarmsCount on Mapper

--- SmartFreeze/Profiles/SiteProfile.cs
+++ SmartFreeze/Profiles/SiteProfile.cs
@@ -11,11 +11,15 @@ namespace SmartFreeze.Profiles
         {
             CreateMap<Site, SiteDetailsDto>()
                 .ForMember(d => d.Latitude, opt => opt.MapFrom(s => s.Position.Latitude))
-                .ForMember(d => d.Longitude, opt => opt.MapFrom(s => s.Position.Longitude));
+                .ForMember(d => d.Longitude, opt => opt.MapFrom(s => s.Position.Longitude))
+                .ForMember(d => d.Altitude, opt => opt.MapFrom(s => s.Position.Altitude))
+                .ForMember(d => d.HasActiveAlarms, opt => opt.MapFrom(s => s.Devices.Any(d => d.Alarms.Any(a => a.IsActive))))
+                .ForMember(d => d.ActiveAlarmsCount, opt => opt.MapFrom(s => s.Devices.SelectMany(d => d.Alarms).Count()));
 
             CreateMap<Site, SiteOverviewDto>()
                 .ForMember(d => d.Latitude, opt => opt.MapFrom(s => s.Position.Latitude))
                 .ForMember(d => d.Longitude, opt => opt.MapFrom(s => s.Position.Longitude))
+                .ForMember(d => d.Altitude, opt => opt.MapFrom(s => s.Position.Altitude))
                 .ForMember(d => d.HasActiveAlarms, opt => opt.MapFrom(s => s.Devices.Any(d => d.Alarms.Any(a => a.IsActive))))
                 .ForMember(d => d.ActiveAlarmsCount, opt => opt.MapFrom(s => s.Devices.SelectMany(d => d.Alarms).Count()));
 
